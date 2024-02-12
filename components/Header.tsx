e68a6149c95765dd00e909e6a70d7259a64476ce
Derek Prieur <DPrieur123@gmail.com>
2024-02-11 20:43:17 -0500

    update

---
+++ components/Header.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Grid2X2,
+  Heart,
+  LayoutGrid,
+  Search,
+  ShoppingCart,
+  User,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const Header = () => {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const input = e.currentTarget.input.value;
+    router.push(`/search/${input}`);
+  };
+
+  return (
+    <header className="flex flex-col md:flex-row bg-walmart px-10 py-7 space-x-5 items-center">
+      <Link href="/" className="mb-5 md:mb-0">
+        <Image
+          src="https://i.imgur.com/5V4wehM.png"
+          alt="Walmart Icon"
+          width={150}
+          height={150}
+        />
+      </Link>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-white rounded-full flex-1 w-full"
+      >
+        <input
+          name="input"
+          type="text"
+          placeholder="Search everything..."
+          className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm"
+        />
+        <button type="submit" className="">
+          <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer" />
+        </button>
+      </form>
+      <div className="flex space-x-5 mt-5 md:mt-0">
+        <Link
+          href="/"
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+        <Link
+          href="/"
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+        <Link
+          href="/"
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+        <Link
+          href="/"
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+        <Link
+          href="/basket"
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div>
+            <p className="text-xs font-extralight">No Items</p>
+            <p>$0.00</p>
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
